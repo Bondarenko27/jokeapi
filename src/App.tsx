d5c9cab948acab927ea9f27c5f7e4469006510c8
Bondarenko27 <bondarenkokristina27@gmail.com>
2022-01-26 09:39:55 -0500

    typescript and react joke fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
+interface joke{
+
+  error: boolean,
+  category:string,
+  setup:string,
+  delivery:string,
+  type:string,
+  joke:string,
+  flags:any,
+  id:number,
+  safe:boolean,
+  lang:string
+  
+}
 function App() {
+const [jokes,setJokes]= React.useState<joke|null>(null)
+ 
+const FetchJoke = async()=> {
+setJokes(null);
+const Fetch= async ()=> {
+const resp = await fetch(" https://v2.jokeapi.dev/joke/programming",{
+  method:"GET"
+})
+.then(function(response) {
+  return response.json();
+  })
+  .then((response)=>{
+    console.log(response)
+    return response
+});
+return resp
+}
+const res = await Fetch();
+setJokes(res)
+}
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       { jokes !== null ? 
+      <><div className='joke'> {jokes.setup}</div><div className='jokeres'> {jokes.delivery}</div>
+      <div className='jokeres'> {jokes.joke}</div></>
+      : null}
+     <div><button className='submit'onClick={() => FetchJoke()}>Submit</button></div>
+    
     </div>
   );
+
 }
 
 export default App;
