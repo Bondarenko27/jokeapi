574d3c049f966b1d09ddf322fdf752e06a1d113a
unknown <pleroux@skitsc.com>
2022-01-27 09:54:14 -0500

    Some updates , added a loader and merged two functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React,{ useEffect, useState } from 'react';
 import './App.css';
-
-
-
 interface joke{
-
   error: boolean,
   category:string,
   setup:string,
@@ -20,92 +16,76 @@ interface joke{
 function App() {
   
 const [loading, setLoading] = useState(false);
-
 const [jokes,setJokes]= React.useState<joke|null>(null)
 const [two,setTwo]= React.useState<joke|null>(null)
- 
+
 const FetchJoke = async()=> {
-  setLoading(true);
-setJokes(null);
-const Fetch= async ()=> {
-const resp = await fetch(" https://v2.jokeapi.dev/joke/programming",{
-  method:"GET"
-})
-
-.then(function(response){
-  return response.json();
-  
-  })
-
-  .then((response)=>{
-    console.log(response)
-    return response
-});
-
-return resp;
-
+    setLoading(true);
+    setJokes(null);
+    setTwo(null)
+    const Fetch= async ()=> {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        const resp = await fetch(" https://v2.jokeapi.dev/joke/programming",{
+        method:"GET"
+        })
+        .then(function(response){
+        return response.json();
+        
+        })
+        .then((response)=>{
+            console.log(response)
+            return response
+        });
+    return resp;
+    }
+    const FetchJoketwo= async ()=> {
+        const resp = await fetch(" https://v2.jokeapi.dev/joke/programming",{
+          method:"GET"
+        })
+        .then(function(response) {
+          return response.json();
+          })
+          .then((response)=>{
+            console.log(response)
+            return response
+        });
+        return resp
+        }
+    const res = await Fetch();
+    const restwo = await FetchJoketwo();
+    setJokes(res)
+    setTwo(restwo)
+    setLoading(false)
 }
-const res = await Fetch();
-setJokes(res)
-setLoading(false)
-}
-
-
-const FetchSecond = async()=> {
-  setLoading(true);
-  setTwo(null);
-  const Fetch= async ()=> {
-  const resp = await fetch(" https://v2.jokeapi.dev/joke/programming",{
-    method:"GET"
-  })
-  .then(function(response) {
-    return response.json();
-    })
-    .then((response)=>{
-      console.log(response)
-      return response
-  });
-  return resp
-  }
-  const res = await Fetch();
-  setTwo(res)
-  setLoading(false)
-  }
-
-
-
-
   return (
     <div className="App">
 
  <div className="FirstDiv">
-    <>{loading ?(<p>Loading ...</p>)/*: (<div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div> )*/: null} 
-       { jokes!== null ?  
+    <>{loading ?
+    <div className='loader'></div>  : null} 
+       {jokes !== null ?  
       <> 
-      <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-       <div className='joke'> {jokes.setup}</div><div className='jokeres'> {jokes.delivery}</div>
-      <div className='jokeres'> {jokes.joke}</div></>
-
-
-
-
-      : null}
+        <div className='joke'>{jokes.setup}</div>
+        <div className='jokeres'> {jokes.delivery}</div>
+        <div className='jokeres'> {jokes.joke}</div></>
+        : null}
 
       </>
-      </div>
-      <div className="SecondDiv">
-      <>{loading ?(<p>Loading ...</p>)/*: (<div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div> )*/:true}  
+ </div>
+
+<div className="SecondDiv">
+      {loading ? <div className='loader'></div> : null}  
         { two!== null ? 
-          <> 
-      <div className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-      <div className='jok'> {two.setup}</div><div className='jokere'> {two.delivery}</div>
-      <div className='jokere'> {two.joke}</div></>
+         <>
+            <div className='jok'> {two.setup}</div>
+            <div className='jokere'> {two.delivery}</div>
+            <div className='jokere'> {two.joke}</div>
+        </>
       : null}
-     </>
-     </div>
+</div>
 
      
-     <div><button className='submit'onClick={() =>{FetchJoke();FetchSecond()} }> Submit </button></div>
+     <div><button className='submit'onClick={() =>FetchJoke()}> Submit </button></div>
     
     </div>
   );
